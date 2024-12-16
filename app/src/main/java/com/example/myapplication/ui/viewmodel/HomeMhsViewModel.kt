d98351f1f8361cb5cb085727bf9bb3ea6c8f98ba
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 14:26:30 +0700

    membuat file baru dengan nama HomeMhsViewModel serta membuat class

---
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.repository.RepositoryMhs
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+
+}
