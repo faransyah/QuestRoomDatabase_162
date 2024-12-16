2e55508a5f74b829df94c3e63e4b3ef3c2841429
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 19:33:13 +0700

    membuat card

--- app/src/main/java/com/example/myapplication/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/myapplication/ui/view/mahasiswa/DetailMhsView.kt
@@ -2,8 +2,12 @@ package com.example.myapplication.ui.view.mahasiswa
 
 import android.app.AlertDialog
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -11,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.data.entity.Mahasiswa
 
 @Composable
@@ -18,7 +23,22 @@ fun ItemDetailMhs(
     modifier: Modifier = Modifier,
     mahasiswa: Mahasiswa
 ){
+Card(
+    modifier = modifier
+        .fillMaxWidth(),
+    colors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+    )
+) {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+        Spacer(modifier = Modifier.padding(4.dp))
 
+    }
+}
 }
 @Composable
 fun ComponentDetailMhs(
