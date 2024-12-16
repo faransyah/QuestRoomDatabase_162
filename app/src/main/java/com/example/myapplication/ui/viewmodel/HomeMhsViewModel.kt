d615c8a0740d9caf0ee80ab85fd7c4bd3e454caa
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 14:35:12 +0700

    membuat data class HomeUiState

--- app/src/main/java/com/example/myapplication/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,18 +1,24 @@
 package com.example.myapplication.ui.viewmodel
 
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myapplication.data.entity.Mahasiswa
 import com.example.myapplication.repository.RepositoryMhs
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel(){
 
-    val homeUIState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
         .filterNotNull()
         .map {
             HomeUiState(
@@ -24,4 +30,26 @@ class HomeMhsViewModel (
             emit(HomeUiState(isLoading = true))
             delay(900)
         }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
 }
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
