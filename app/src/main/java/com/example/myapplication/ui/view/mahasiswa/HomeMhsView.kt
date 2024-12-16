734d5cbe5875187b0652ca3c6a43e59874dcb9d0
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 18:59:57 +0700

    membuat fungsi ListMahasiswa serta fungsi BodyHomeMhsView

--- app/src/main/java/com/example/myapplication/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/myapplication/ui/view/mahasiswa/HomeMhsView.kt
@@ -5,20 +5,57 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.viewmodel.HomeUiState
 
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    when   {}
+}
+
+
+@Composable
+fun ListMahasiswa(
+    listMhs: List<Mahasiwa>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {}
+){
+    LazyColumn(
+        modifier =modifier
+    ) {
+        items(
+            items = listMhs,
+            itemContent = { mhs ->
+                CardMhs(
+                    mhs = mhs,
+                    onClick = { onClick(mhs.nim)}
+                )
+            }
+        )
+    }
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardMhs(
@@ -66,7 +103,7 @@ fun CardMhs(
                     Spacer(modifier = Modifier.padding(4.dp))
                     Text(
                         text = mhs.kelas,
-                        fontWeight = FontWeight.Bold
+                        fontWeight = FontWeight.Bold,
                     )
                 }
             }
