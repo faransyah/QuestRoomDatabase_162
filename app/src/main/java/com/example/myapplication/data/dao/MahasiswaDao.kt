ffaa72c1bd6d931aa5da84c88dd255ed9638c7d7
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 14:02:15 +0700

    menambahkan pada bagian mahasisadao

--- app/src/main/java/com/example/myapplication/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/myapplication/data/dao/MahasiswaDao.kt
@@ -1,11 +1,34 @@
 package com.example.myapplication.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.myapplication.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    // getAllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    // getMahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa (nim: String): Flow<Mahasiswa>
+
+    // deleteMahasiswa
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    // updateMahasiswa
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
 }
+
+
