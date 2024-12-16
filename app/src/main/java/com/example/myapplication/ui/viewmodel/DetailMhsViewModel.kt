5820dff8f2f7a627fe813715d0b90844b3fcd171
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 17:23:57 +0700

    membuat beberapa variabel

--- app/src/main/java/com/example/myapplication/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,9 +1,21 @@
 package com.example.myapplication.ui.viewmodel
 
+import com.example.myapplication.ui.theme.viewmodel.MahasiswaEvent
+
 class DetailMhsViewModel (
 savedSyaye
 )
 
 data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
 
-)
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
