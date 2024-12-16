55b478416566f94f4087c290083bf0842eb8aa30
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 14:29:59 +0700

    membuat variabel homeuiState

--- app/src/main/java/com/example/myapplication/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,9 +2,26 @@ package com.example.myapplication.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel(){
 
+    val homeUIState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
 }
