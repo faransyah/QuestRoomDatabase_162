06764ec3df41152f673af6422d99bab0b86cdb9e
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 17:36:15 +0700

    mengisi class UpdateMhsViewModel

---
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.repository.RepositoryMhs
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+
+}
