0757630a3f7ac0bc71aaf45ae1d5e7610c5b1e3a
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 20:18:06 +0700

    membuat isi HomeMhsView

--- app/src/main/java/com/example/myapplication/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/myapplication/ui/navigation/PengelolaHalaman.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.view.mahasiswa.DestinasiInsert
+import com.example.myapplication.ui.view.mahasiswa.HomeMhsView
 import com.example.myapplication.ui.view.mahasiswa.InsertMhsView
 
 @Composable
@@ -17,10 +18,19 @@ fun PengelolaHalaman(
 ){
     NavHost(navController = navController, startDestination = DestinasiHome.route) {
         composable(
-            route = DestinasiInsert.route
+            route = DestinasiHome.route
         ) {
-            InsertMhsView(
-                onBack = {}, onNavigate = {}
+            HomeMhsView(
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinationDetail.route}/$nim")
+                    println(
+                        "PengelolaHalaman: nim = $nim"
+                    )
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = modifier
             )
         }
     }
