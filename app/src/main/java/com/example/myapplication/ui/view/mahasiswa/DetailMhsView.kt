a0bf488b83fa6f88b5fc9e9e49e12e9e1a77db64
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 19:28:51 +0700

    mengisi text

--- app/src/main/java/com/example/myapplication/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/myapplication/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication.ui.view.mahasiswa
 
 import android.app.AlertDialog
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun ComponentDetailMhs(
@@ -13,7 +18,21 @@ fun ComponentDetailMhs(
     judul: String,
     isinya: String
 ){
-
+Column(
+    modifier = modifier.fillMaxWidth(),
+    horizontalAlignment = Alignment.Start
+) {
+    Text(
+        text = "$judul : ",
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Gray
+    )
+    Text(
+        text = isinya, fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
 }
 @Composable
 private fun DeleteConFirmationDialog(
