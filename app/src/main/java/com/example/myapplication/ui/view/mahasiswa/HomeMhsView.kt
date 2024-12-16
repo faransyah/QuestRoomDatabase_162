59653e189c9f49b1775c0c7814f1bfc5f0df31fc
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-16 19:12:47 +0700

    membuat floatingactionbutton

--- app/src/main/java/com/example/myapplication/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/myapplication/ui/view/mahasiswa/HomeMhsView.kt
@@ -14,7 +14,9 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -44,8 +46,18 @@ fun HomeMhsView(
     Scaffold(
         topBar = {
             TopAppBar(
-                jydy
+                judul = "Daftar Mahasiswa",
+                showBackButton = false,
+                onBack = {},
+                modifier = modifier
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes.medium,,
+                modifier = Modifier.padding(16.dp)
+            ) { }
         }
     ) {  }
 }
